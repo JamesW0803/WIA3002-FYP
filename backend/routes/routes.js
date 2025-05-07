3747b42d39fc1cfcfb7ad284fd126838e7a88513
James Wong <jwyn0803@gmail.com>
2025-05-08 01:15:08 +0800

    Fix race condition
    Replace .then()/.catch() with wait to prevent the user see dashboard even if login fails

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -8,7 +8,7 @@ const { authenticate } = require("../middleware/authMiddleware");
 const router = express.Router();
 
 router.use("/user", userRoutes);
-router.use("/courses", authenticate , courseRoutes);
+router.use("/courses", authenticate, courseRoutes);
 router.use("/student", authenticate, studentRoutes);
 router.use("/curriculums", curriculumRoutes);
 
--- frontend/src/api/axiosClient.js
+++ frontend/src/api/axiosClient.js
@@ -1,35 +1,36 @@
-import axios from 'axios';
+import axios from "axios";
 
 // Create an axios instance
 const axiosClient = axios.create({
-    baseURL: process.env.REACT_APP_BASE_URL
-})
+  baseURL: process.env.REACT_APP_BASE_URL,
+});
 
 // Intercept requests
-axiosClient.interceptors.request.use( 
-    (config) => {
-        const token = localStorage.getItem('token');
-        if(token){
-            config.headers['Authorization'] = `Bearer ${token}`;
-        }
-        return config;
-    },
-    (error) => {
-        return Promise.reject(error);
+axiosClient.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers["Authorization"] = `Bearer ${token}`;
     }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
 );
 
 // Intercept responses
 axiosClient.interceptors.response.use(
-    (response) => {
-        // Any status code that lie within the range of 2xx cause this function to trigger
-        // Do something with response data
-        return response;
-    },  
-    (error) => {
-        // Any status codes that falls outside the range of 2xx cause this function to trigger
-        // Do something with response error
-    return Promise.reject(error)}
+  (response) => {
+    // Any status code that lie within the range of 2xx cause this function to trigger
+    // Do something with response data
+    return response;
+  },
+  (error) => {
+    // Any status codes that falls outside the range of 2xx cause this function to trigger
+    // Do something with response error
+    return Promise.reject(error);
+  }
 );
 
 export default axiosClient;
