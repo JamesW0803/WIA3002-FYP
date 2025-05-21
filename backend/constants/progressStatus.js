f6b512462f25ab37328be61f1ec6a579c1d59029
NKHon831 <khaihon030831@gmail.com>
2025-05-21 12:31:21 +0800

    feat: set placeholder for null properties

---
+++ backend/constants/progressStatus.js
@@ -0,0 +1,19 @@
+const ON_TRACK = "On track";
+const AT_RISK = "At risk";
+const UNKNOWN = "Unknown";
+const DELAYED = "Delayed";
+
+const PROGRESS_STATUS = {
+    ON_TRACK,
+    DELAYED,
+    AT_RISK,
+    UNKNOWN
+}
+
+module.exports = {
+    ON_TRACK,
+    DELAYED,
+    AT_RISK,
+    UNKNOWN,
+    PROGRESS_STATUS
+}
--- backend/controllers/studentController.js
+++ backend/controllers/studentController.js
@@ -1,9 +1,11 @@
 const Student = require("../models/Student");
+const { formatStudents } = require("../utils/formatter/studentFormatter");
 
 const getAllStudents = async (req, res) => {
     try{
-        const students = await Student.find()
-        res.status(200).json(students)
+        const students = await Student.find().populate("programme")
+        const formattedStudents = formatStudents(students)
+        res.status(200).json(formattedStudents)
     }catch(error){
         res.status(500).json({message: error.message})
         console.log("Error fetching students: ", error)
